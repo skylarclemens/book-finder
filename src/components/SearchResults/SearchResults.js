313180fae56177bb8a3f8497bfaa3e4ede7cf98b
Skylar <skylarc55@gmail.com>
2022-12-08 18:38:05 -0500

    Add routing, Book page, and updated styling

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,22 @@
 import './SearchResults.scss';
+import { Link } from 'react-router-dom';
 
 const SearchResults = ({ books }) => {
   return (
     <ul className={`searchResults ${books.length ? '' : 'empty'}`}>
       {books.map((book) => (
-          <li className="book-item" key={book.id}>
-            <div className="book-image-container">
-              <img className="book-image" src={book.volumeInfo.imageLinks.smallThumbnail} />
-            </div>
-            <div className="book-info">
-              <span className="title">{book.volumeInfo.title}</span>
-              <span>{book.volumeInfo.authors[0]}</span>
-            </div>
+          <li key={book.id}>
+            <Link to={`/book/${book.id}`}>
+              <div className="book-item">
+                <div className="book-image-container">
+                  <img className="book-image" src={book.volumeInfo.imageLinks.smallThumbnail} alt={`${book.volumeInfo.title} cover`} />
+                </div>
+                <div className="book-info">
+                  <span className="title">{book.volumeInfo.title}</span>
+                  <span>{book.volumeInfo.authors[0]}</span>
+                </div>
+              </div>
+            </Link>
           </li>
         )
       )}
